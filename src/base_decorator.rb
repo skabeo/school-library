20dcac75d7538874c2e829d45c66ab4ff2e8871c
skabeo <spencerokyere95@gmail.com>
2023-06-20 13:01:51 +0100

    Add base decorator class

---
+++ src/base_decorator.rb
@@ -0,0 +1,13 @@
+require_relative 'nameable'
+
+class BaseDecorator < Nameable
+  def intialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+
