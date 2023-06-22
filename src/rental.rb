03314fc70418bb53bd5623f70c1fe2a102af9bfc
skabeo <spencerokyere95@gmail.com>
2023-06-22 16:55:32 +0100

    Modify rental class

--- src/rental.rb
+++ src/rental.rb
@@ -1,11 +1,11 @@
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date, book, person)
+  def initialize(date, person, book)
     @date = date
     @book = book
     @person = person
-    @book.add_rental(self)
-    @person.add_rental(self)
+    person.rentals << self
+    book.rentals << self
   end
 end
