e27235021faafc97c83980957b19edeebbefa261
skabeo <spencerokyere95@gmail.com>
2023-06-22 07:30:54 +0100

    Add classroom method

--- src/student.rb
+++ src/student.rb
@@ -12,4 +12,9 @@ class Student < Person
   def play_hooky
     '¯\(ツ)/¯'
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 end
